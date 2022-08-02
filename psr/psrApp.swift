cf676d7a9b85b98fae1498fca446d4bafb9618ea
Nando <fernando.deleon@gmail.com>
2022-08-02 19:20:48 +1000

    Initial Commit

---
+++ psr/psrApp.swift
@@ -0,0 +1,17 @@
+//
+//  psrApp.swift
+//  psr
+//
+//  Created by Fernando De Leon on 2/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct psrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
